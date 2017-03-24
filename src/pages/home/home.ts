abd255e93c65258728780b7868e215773bcddf96
Nikita <garroshik@gmail.com>
2017-03-24 18:18:30 +0200

    localstorage test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   value:string ='';
+  counter:number = 0;
   constructor(public navCtrl: NavController) {
     
 
   }
   setLocal(){
-    localStorage.setItem('id', this.value)
-    console.log(this.value)
+    var a = [1,3,4]
+    var b = JSON.stringify(a)
+    localStorage.setItem('id_'+this.counter, b)
+    console.log(JSON.parse(b))
+    this.counter++
   }
 
 
